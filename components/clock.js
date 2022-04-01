1796074745f771bcc31ad53ad4ba80cb750ac3b3
AleksasN <aleksasne@gmail.com>
2022-04-01 13:25:42 +0300

    update(date+calender+loader)

--- components/clock.js
+++ components/clock.js
@@ -13,3 +13,17 @@ document.querySelector(".clock").innerText = h + ":" + m ;
 }
 setTimeout(currentTime,1000)
 let timeInterval = setInterval(currentTime, 1000);
+
+
+const date = new Date();
+const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
+const month = date.toLocaleString("eu-Lt", {month: 'long'})
+const day = date.toLocaleString("eu-Lt", {day: 'numeric'})
+const year = date.toLocaleString("eu-Lt", {year: 'numeric'})
+const weekday = date.toLocaleString("eu-Lt", {weekday: 'long'})
+console.log(month);
+console.log(day);
+console.log(year);
+console.log(weekday);
+document.querySelector('.current-date').innerHTML = `${weekday}, ${month}, ${day}d., ${year}m.`;
+console.log(today);
